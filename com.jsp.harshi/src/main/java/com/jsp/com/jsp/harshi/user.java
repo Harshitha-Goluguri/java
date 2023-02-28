5a7b3a3b97c97a41c16aff396b347fad08587e63
hp <hp@LAPTOP-PTNTARIU>
2023-02-28 18:12:47 +0530

    harshi

---
+++ com.jsp.harshi/src/main/java/com/jsp/com/jsp/harshi/user.java
@@ -0,0 +1,29 @@
+package com.jsp.com.jsp.harshi;
+
+public class user {
+	@id
+	private String Name;
+	private int Id;
+	private int age;
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	public int getId() {
+		return Id;
+	}
+	public void setId(int id) {
+		Id = id;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	
+
+}
